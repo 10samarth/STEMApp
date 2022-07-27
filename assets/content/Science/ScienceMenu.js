430663e045dec9d5d91f2a7a5b2c4c7ccbc970f6
Vijaywargia <Saurabh.Vijaywargia@cdk.com>
2022-07-27 16:06:21 -0700

    Saurabh Sameer changes

--- assets/content/Science/ScienceMenu.js
+++ assets/content/Science/ScienceMenu.js
@@ -44,7 +44,6 @@ const sciData = [
         youtube: "Ok8JDXSYw1U",
         subtitle: "1856-1943",
     },
-
     {
       name: "Chien-Shiung Wu",
       avatar_url:
@@ -64,6 +63,101 @@ const sciData = [
         youtube: "Et4cY9msOWw",
         subtitle: "1912-1997",
     },
+    {
+      name: "Asima Chatterjee",
+      avatar_url:
+        "https://upload.wikimedia.org/wikipedia/en/7/7d/Asima_Chatterjee_1.jpg",
+      birthDate: "23 September 1917",
+      deathDate: "22 November 2006 (aged 89)",
+      awards: ["C.V Raman award","P.C Ray Award","the S.S Bhatnagar award"],
+      summary:"Asima Chatterjee was born on 23-09-1917 in Kolkata in the state of West Bengal, India. She was an Indian Chemist, Professor & Scientist. She was an organic chemist noted for her work in the fields of organic chemistry and phytomedicine. She also authored a considerable volume of work on medicinal plants of the Indian subcontinent. She was the first woman to receive a Doctorate of Science from an Indian university. As she was the first female to receive a doctorate and even started a chemistry department in the Lady Brabourne College of the University of Calcutta she inspired a lot of women in that time of India to pursue their dreams and work as innovators and specialists.",
+      cardA:"She was born in a traditional Bengali film. She was excellent in her academics and after completion of her schooling; she obtained graduation degree from Scottish Church College in chemistry honors. She then obtained her post-graduate degree in organic chemistry and after which, she completed Ph.D. in 1944. Her research primarily focused on synthetic organic chemistry and chemistry of plant products. Her research revolved around chemistry and it led to the invention of chemotherapy, anti-malarial, and anti-convulsive drugs.",
+      cardB:"",
+      imageWork:"",
+      imageLegacy:"",
+      links:[
+        "https://en.wikipedia.org/wiki/Asima_Chatterjee",
+        "https://www.youtube.com/watch?v=WJyfNQcrwJ4",
+      ],
+      youtube: "WJyfNQcrwJ4",
+      subtitle: "1917-2006"
+    },
+    {
+      name: "Asima Chatterjee",
+      avatar_url:
+        "https://upload.wikimedia.org/wikipedia/en/7/7d/Asima_Chatterjee_1.jpg",
+      birthDate: "23 September 1917",
+      deathDate: "22 November 2006 (aged 89)",
+      awards: ["C.V Raman award","P.C Ray Award","the S.S Bhatnagar award"],
+      summary:"Asima Chatterjee was born on 23-09-1917 in Kolkata in the state of West Bengal, India. She was an Indian Chemist, Professor & Scientist. She was an organic chemist noted for her work in the fields of organic chemistry and phytomedicine. She also authored a considerable volume of work on medicinal plants of the Indian subcontinent. She was the first woman to receive a Doctorate of Science from an Indian university. As she was the first female to receive a doctorate and even started a chemistry department in the Lady Brabourne College of the University of Calcutta she inspired a lot of women in that time of India to pursue their dreams and work as innovators and specialists.",
+      cardA:"She was born in a traditional Bengali film. She was excellent in her academics and after completion of her schooling; she obtained graduation degree from Scottish Church College in chemistry honors. She then obtained her post-graduate degree in organic chemistry and after which, she completed Ph.D. in 1944. Her research primarily focused on synthetic organic chemistry and chemistry of plant products. Her research revolved around chemistry and it led to the invention of chemotherapy, anti-malarial, and anti-convulsive drugs.",
+      cardB:"None",
+      imageWork:"",
+      imageLegacy:"",
+      links:[
+        "https://en.wikipedia.org/wiki/Asima_Chatterjee",
+        "https://www.youtube.com/watch?v=WJyfNQcrwJ4",
+      ],
+      youtube: "WJyfNQcrwJ4",
+      subtitle: "1917-2006"
+    },
+	
+	{
+      name: "Mary Anning",
+      avatar_url:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mary_Anning_painting.jpg/440px-Mary_Anning_painting.jpg",
+      birthDate: "21 May 1799",
+      deathDate: "9 Mar 1847 (aged 89)",
+      awards: ["Advancement of Science and the British government to award her an annuity"],
+      summary:"She was an English fossil collector, dealer, and palaeontologist who became known around the world for the discoveries she made in Jurassic marine fossil beds in the cliffs along the English Channel at Lyme Regis in the county of Dorset in Southwest England. Anning's findings contributed to changes in scientific thinking about prehistoric life and the history of the Earth.",
+      cardA:"Anning searched for fossils in the area's Blue Lias and Charmouth Mudstone cliffs, particularly during the winter months when landslides exposed new fossils that had to be collected quickly before they were lost to the sea. Her discoveries included the first correctly identified ichthyosaur skeleton when she was twelve years old;[1] the first two nearly complete plesiosaur skeletons; the first pterosaur skeleton located outside Germany; and fish fossils. Her observations played a key role in the discovery that coprolites, known as bezoar stones at the time, were fossilised faeces, and she also discovered that belemnite fossils contained fossilised ink sacs like those of modern cephalopods.",
+      cardB:"Anning's first famous discovery was made shortly after her father's death when she was still a child of about 12. In 1811 (some sources say 1810 or 1809) her brother Joseph found a 4 ft (1.2 m) skull, but failed to locate the rest of the animal.[22] After Joseph told Anning to look between the cliffs at Lyme Regis and Charmouth, she found the skeleton—17 ft (5.2 m) long in all—a few months later. The family hired workmen to dig it out in November that year, an event covered by the local press on 9 November, who identified the fossil as a crocodile.",
+      imageWork:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Anning_1st_ichthyosaur_skeleton.jpg/440px-Anning_1st_ichthyosaur_skeleton.jpg",
+      imageLegacy:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Macrocephalus.jpg/440px-Macrocephalus.jpg",
+      links:[
+        "https://en.wikipedia.org/wiki/Mary_Anning",
+        "https://www.youtube.com/watch?v=a-CW0B4YeBQ",
+      ],
+      youtube: "a-CW0B4YeBQ",
+      subtitle: "1917-2006"
+    },
+    {
+      name: "Ada Lovelace",
+     avatar_url:
+"https://en.wikipedia.org/wiki/Ada_Lovelace#/media/File:Ada_Byron_daguerreotype_by_Antoine_Claudet_1843_or_1850.jpg",
+      birthDate: "10 December 1815",
+      deathDate: "27 November 1852 (aged 37)",
+      awards: ["None"],
+      summary:" Ada Lovelace was an English Mathematician who is regarded as the world’s first computer programmer! In 1836, she wrote the world’s first computer program known as an algorithm for an Analytical Engine to compute Bernoulli numbers. In 1852, Lovelace died due to an illness at only 37 years old. However, despite her short life, she made significant contributions to the fields of mathematics and computer science that can still be seen to this day. She is still regarded as the first person to understand the potential of a computing machine. Without the contributions of Ada Lovelace, the modern computer and associated algorithms that we take for granted may not exist ‘The more I study, the more insatiable do I feel my genius for it to be.-	Ana Lovelace.",
+      cardA:" In June 1833 Lovelace’s tutor, Mary Somerville, introduced her to British mathematician, philosopher, and inventor Charles Babbage, now widely considered to have been the “father of the computer.” Lovelace became fascinated with Babbage’s groundbreaking work on his mechanical calculating device, he called the Analytical Engine.In 1842, Babbage asked Lovelace to translate from French into English a scholarly article on his calculating machine written by Italian military engineer Luigi Menabrea. Ada not only translated the article, but she also supplemented it with an elaborate analytical section she simply titled “Notes,” comprised of Note A to Note G. Lovelace’s seven notes, now revered as a milestone in the history of computers, contained what many consider to have been the first computer program—a structured set of instructions to be carried out by a machine. In her Note G, Lovelace describes an algorithm that would instruct Babbage’s Analytical Engine to accurately compute Bernoulli numbers. Today it is considered to have been the first algorithm specifically created to be implemented on a computer, and the reason Lovelace is often called the first computer programmer. Since Babbage never completed his Analytical Engine, Lovelace’s program was never tested. However, her process for having a machine repeat a series of instructions, called “looping,” remains a staple of computer programming today.",
+      cardB:" The computer language Ada, created on behalf of the United States Department of Defense, was named after Lovelace. The reference manual for the language was approved on 10 December 1980 and the Department of Defense Military Standard for the language, MIL-STD-1815, was given the number of the year of her birth. Since 1998 the British Computer Society (BCS) has awarded the Lovelace Medal,[92] and in 2008 initiated an annual competition for women students. BCSWomen sponsors the Lovelace Colloquium, an annual conference for women undergraduates. Ada College is a further-education college in Tottenham Hale, London focused on digital skills",
+      imageWork:" https://images.squarespace-cdn.com/content/v1/54905286e4b050812345644c/1615334543833-32GHU3RG0SAY0BD28IGZ/ada-lovelace-day-commemorating-the-worlds-first-computer-programmer.png?format=1500w",
+      imageLegacy:" https://www.earlham.ac.uk/sites/default/files/styles/image_mobile_1x/public/images/articles/Ada%20Lovelace%20inspired/ada-lovelace-inspired-inspiring-analytical-engine.jpg?itok=14DdaBwH",
+      links:[
+        "https://en.wikipedia.org/wiki/Ada_Lovelace", "https://en.wikipedia.org/wiki/Bernoulli_number", "https://www.thoughtco.com/thmb/WCuDeiqnKIsMN3YVwIRdqdRk16E=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ada-a77bc6f9821047e0acb8ccad86bc47f4.jpg"
+      ],
+     youtube: " uBbVbqRvqTM",
+      subtitle: ""
+    },
+{
+      name: "Mae Carol Jemison",
+      avatar_url:
+        " https://images.squarespace-cdn.com/content/v1/54905286e4b050812345644c/1615401236074-7BUTAZBHO81Q7QZ519AL/mae-jemison-2.jpg?format=2500w",
+
+      birthDate: "17 October 1956",
+      deathDate: "",
+      awards: ["Sylvaus Thayer Award","Elizabeth Blackwell Medal"], 
+      summary:" Mae Carol Jemison is an incredibly talented and inspirational figure. As a decorated engineer, physician, mathematician, and NASA astronaut, she is best known for becoming the first African American woman to go to outer space, which happened when she was a member of the Endeavour Space Shuttle that launched into orbit in 1992. Before becoming an astronaut, Jemison served in the Peace Corps as a general practice physician. Today, she serves on the council for the Science Matters initiative, with the goal of encouraging children to pursue STEM careers at a young age. Never limit yourself because of others' limited imagination; never limit others because of your own limited imagination. -	Mae Carol Jemison",
+      cardA:" After working with the Peace Corps, Jemison opened a private practice as a doctor. However, once Sally Ride became the first American woman in space in 1983, Jemison decided to apply to the astronaut program at NASA. She applied in 1985, but after the Space Shuttle Challenger exploded in 1986, NASA took a break from accepting new people. However, Jemison applied again in 1987 and was one of the 15 people chosen out of over 2,000 applications. She was selected for NASA Astronaut Group 12, which was the first group chosen after the Challenger explosion. After being selected, Jemison trained with NASA and worked on projects at the Kennedy Space Center in Florida and the Shuttle Avionics Integration Laboratory. She received her first mission on September 28, 1989 when she was selected to join the STS-47 crew as a Mission Specialist. On September 12, 1992 Jemison and six other astronauts went into space on the space shuttle Endeavor. This voyage made Jemison the first African American woman in space. The team made 127 orbits around the Earth and returned to the Kennedy Space Center in Florida on September 20, 1992.",
+      cardB:" Jemison faced many roadblocks to her dreams of pursuing science. Once, when Jemison said she wanted to be a scientist when she grew up, her teacher asked if she meant a nurse. She later founded The Earth We Share (TEWS), an international science camp that encourages science literacy for all. She was on an episode of Star Trek! Jemison began each of her shifts in space by saying, “Hailing frequencies open,” a reference to Lieutenant Uhura on Star Trek. In 1993, Jemison appeared in her own episode of Star Trek: The Next Generation as Lieutenant Palmer. She has also written several books and appeared on many television programs including an episode of Star Trek: The Next Generation. Currently, Jemison is leading the 100 Year Starship project through the United States Defense Advanced Research Projects Agency (DARPA). This project works to make sure human space travel to another star is possible within the next 100 years. She also serves on the Board of Directors for many organizations including; the Kimberly-Clark Corp., Scholastic, Inc., Valspar Corp., Morehouse College, Texas Medical Center, Texas State Product Development and Small Business Incubator, Greater Houston Partnership Disaster Planning and Recovery Task Force, and the National Institute of Biomedical Imaging and Bioengineering. Jemison is a member of the National Academy of Sciences’ Institute of Medicine, and has been inducted into the National Women’s Hall of Fame, National Medical Association Hall of Fame and Texas Science Hall of Fame. She has received multiple awards and honorary degrees including the National Organization for Women’s Intrepid Award and the Kilby Science Award.",
+      imageWork:" https://cdn.britannica.com/04/1804-004-1457B2DB/Atlantis-Mae-Jemison-preflight-switch-test-crew-1989.jpg",
+      imageLegacy:" https://0901.static.prezi.com/preview/v2/ggav7x5bagqj45jgpvgc4xfwkd6jc3sachvcdoaizecfr3dnitcq_3_0.png",
+      links:[
+        " https://en.wikipedia.org/wiki/Mae_Jemison"
+      ],
+      youtube: " NL_KtO80l7w",
+      subtitle: ""
+    }
   ];
 
  export default sciData;
