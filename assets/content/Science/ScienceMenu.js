96eb10b67c9d3e2d2fb4bc6a672ca9dd1d02755b
SergioGutierrez <sergio.gutierrez@sjsu.edu>
2022-07-27 12:33:04 -0800

    Added N Tesla

--- assets/content/Science/ScienceMenu.js
+++ assets/content/Science/ScienceMenu.js
@@ -23,7 +23,13 @@ const sciData = [
       name: "Rosalind Franklin",
       avatar_url:
         "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
-      subtitle: "1920-1958",
+      subtitle: "1920-1959",
+    },
+    {
+      name: "Nikola Tesla",
+      avatar_url:
+        "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
+      subtitle: "1920-1959",
     },
   ];
 
